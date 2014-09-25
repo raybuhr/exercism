84350570299a3d6bfe35250f74847ea9350beff6
raybuhr <rebuhr@gmail.com>
2014-09-25 16:36:38 -0500

    v2
    2nd version, without class just function

--- bob.py
+++ bob.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 24 16:31:02 2014
-
+Last modified on Fri Sep 25 16:32:24 2014
 @author: rebuhr
 """
 
-class Bob(object):
-    def __init__(self, hey):
-        hey = raw_input()
-        if hey == hey.upper():
-            return 'Whoa, chill out!'
-        elif '?' in hey:
-            return "Sure."
-        elif hey == '':
-            return "Fine. Be that way!"
-        elif hey is None:
-            return "Fine. Be that way!"
-        elif hey == '    \t':
-            return "Fine. Be that way!"
-        else:
-            return "Whatever."
+def hey(msg):
+    if msg.isupper():
+        return 'Whoa, chill out!'
+    elif msg.endswith('?'):
+        return "Sure."
+    elif msg == '':
+        return "Fine. Be that way!"
+    elif msg is None:
+        return "Fine. Be that way!"
+    elif msg == '    \t':
+        return "Fine. Be that way!"
+    else:
+        return "Whatever."
