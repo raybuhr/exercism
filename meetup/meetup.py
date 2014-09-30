6c9bdec6128a5b3ca7a2e14639e384c1228be5e9
raybuhr <rebuhr@gmail.com>
2014-09-30 17:12:00 -0500

    meetup
    added meetup project

---
+++ meetup/meetup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 30 10:26:07 2014
+
+@author: rebuhr
+"""
+
+# Calculates the date of meetups.
+from datetime import date
+
+def meetup_day(year, month, day_of_week, meetup_type):
+    
